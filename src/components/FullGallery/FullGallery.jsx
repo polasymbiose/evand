47e4842081f20316a9437d3927f6eafe789e9460
Paul Mura <p.mura@Pauls-MacBook-Pro.local>
2019-11-25 07:29:28 +0100

    remove gallery slider

--- src/components/FullGallery/FullGallery.jsx
+++ src/components/FullGallery/FullGallery.jsx
@@ -1,24 +1,20 @@
 import React, { useState, useEffect } from 'react'
 import { get } from 'lodash'
-// @ts-ignore
-import Carousel from 'react-awesome-slider'
-// @ts-ignore
-import CarouselStyles from 'react-awesome-slider/src/core/styles.scss'
-// @ts-ignore
+// import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext, Image } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
+// import AwesomeSlider from 'react-awesome-slider'
+// import CarouselStyles from 'react-awesome-slider/src/core/styles.scss'
 import { useHistory, useLocation } from 'react-router-dom'
 import { animated, useSpring } from 'react-spring'
 import Burger from '../Burger/Burger'
-import { Images } from '../GalleryComponent/GalleryComponent'
 import Loader from '../Loader/Loader'
 import './FullGallery.scss'
+import AliceCarousel from 'react-alice-carousel'
+import 'react-alice-carousel/lib/alice-carousel.css'
 
 var classNames = require('classnames')
 
-interface FullGalleryProps {
-  imgs: Images[]
-}
-
-export default function FullGallery({ imgs }: FullGalleryProps) {
+export default function FullGallery({ imgs }) {
   const [loading, setloading] = useState(false)
   const history = useHistory()
   const location = useLocation()
@@ -54,19 +50,65 @@ export default function FullGallery({ imgs }: FullGalleryProps) {
         <Burger toggle={handleClose} open={isOpen} gallery />
       </animated.div>
 
-      <Loader active={loading} />
+      {/* <Loader active={loading} /> */}
 
-      {isOpen && (
-        <Carousel
+      <AliceCarousel mouseTrackingEnabled>
+        {[...imgs].map((item, index) => {
+          return (
+            <div key={item.src} className={'sliderWrapper'}>
+              <div
+                style={{
+                  backgroundImage: `url(${item.src})`,
+                  backgroundPosition: 'center',
+                  backgroundSize: 'contain',
+                  backgroundRepeat: 'no-repeat',
+                }}
+              />
+            </div>
+          )
+        })}
+      </AliceCarousel>
+
+      {/* <CarouselProvider
+        naturalSlideWidth={1000}
+        naturalSlideHeight={1000}
+        totalSlides={imgs.length}
+        currentSlide={findIndex}
+        hasMasterSpinner={false}
+        lockOnWindowScroll={true}
+        infinite={true}
+
+      >
+        <Slider className={'mainGallery'} onChange={(e) => {
+          console.log('e :', e)
+        }}>
+        {[...imgs].map((item, index) => {
+            return (
+              <Slide index={index} key={item.src}>
+                  <Image
+                    src={item.src}
+                    isBgImage={true}
+                    hasMasterSpinner={true}
+                  >
+                    </Image>
+              </Slide>
+            )
+          })}
+        </Slider>
+        <ButtonBack>Back</ButtonBack>
+        <ButtonNext>Next</ButtonNext>
+      </CarouselProvider> */}
+
+
+        {/* <AwesomeSlider
           infiniteLoop
           useKeyboardArrows={true}
           onTransitionStart={() => {}}
-          onTransitionRequest={(e: any) => {
+          onTransitionRequest={e => {
             const next = e.eventName === 'next' ? 1 : -1
             const foo = (e.currentIndex + next) % imgs.length
             const finalIndex = foo < 0 ? imgs.length - 1 : foo
             const n = get(imgs, [finalIndex, 'src'], false)
-            // @ts-ignore
             const name = n && n.split('/').pop().replace('.jpg', '')
             history.push(`/gallery/${name}`)
           }}
@@ -76,7 +118,7 @@ export default function FullGallery({ imgs }: FullGalleryProps) {
           cssModule={CarouselStyles}
           bullets={false}
         >
-          {[...imgs].map((item: Images, index: number) => {
+          {[...imgs].map((item, index) => {
             return (
               <div key={item.src} className={'sliderWrapper'}>
                 <div
@@ -93,8 +135,8 @@ export default function FullGallery({ imgs }: FullGalleryProps) {
               </div>
             )
           })}
-        </Carousel>
-      )}
+        </AwesomeSlider> */}
+      }
     </div>
   )
 }
