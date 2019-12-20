d62611cd73ef812f3f4fcec4d387bab102e82d38
Paul Mura <p.mura@Pauls-MacBook-Pro.local>
2019-12-20 22:42:16 +0100

    init

--- src/components/GalleryComponent/GalleryComponent.tsx
+++ src/components/GalleryComponent/GalleryComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import FullGallery from '../FullGallery/FullGallery';
 import GalleryFilter from '../GalleryFilter/GalleryFilter';
 import Page from '../Page/Page';
@@ -13,19 +13,74 @@ export interface Images {
 }
 
 const GalleryComponent = () => {
+
+  const [json, setjson] = useState()
+  const isMounted = useRef(false)
+  const initJson = useRef<any[]>([])
+  const [imageFilter, setImageFilter] = useState<any[]>([])
+  const [checkbox] = useState(new Map())
+
+  useEffect(() => {
+    isMounted.current = true
+
+    fetch(`${process.env.PUBLIC_URL}/img.json`)
+      .then(async res => {
+        const r = await res.json()
+        const { gallery } = r
+        isMounted.current && setjson(gallery)
+
+        let imgs: any[] = []
+        isMounted.current &&
+          Object.keys(gallery).forEach(key => {
+            const category = gallery[key]
+            imgs = imgs.concat(category)
+          })
+        initJson.current = imgs
+        setImageFilter(imgs)
+      })
+      .catch()
+
+    return () => {
+      isMounted.current = false
+    }
+  }, [])
+
+  const merge = (a: any[], b: any[]) => a.filter(aa => !b.find(bb => aa.src === bb.src)).concat(b)
+
+  const handleChange = (e: any) => {
+    const label = e.target.value
+    const check = checkbox
+    if (check.has(label)) {
+      check.delete(label)
+    } else {
+      check.set(label, json[label])
+    }
+    arrangeFilter()
+  }
+
+  const arrangeFilter = () => {
+    let filter: any[] = []
+    const check = checkbox
+    check.forEach(ar => {
+      filter = merge(filter, ar)
+    })
+    filter.length > 0 ? setImageFilter(filter) : setImageFilter(initJson.current)
+  }
+
+
   return (
     <Page>
       <GalleryFilter
-        render={images => {
-          return (
-          <WideScreenGallery
-            imgs={images}
+      handleChange={handleChange}
+      json={json}
+      checkbox={checkbox}
+      />
+      <WideScreenGallery
+            imgs={imageFilter}
             render={(items: any[]) => (
               <FullGallery imgs={items} />
             )}
           />
-        )}}
-      />
     </Page>
   )
 }
--- src/components/GalleryFilter/GalleryFilter.tsx
+++ src/components/GalleryFilter/GalleryFilter.tsx
@@ -4,63 +4,15 @@ import useMedia from '../../hooks/useMedia'
 const cn = require('classnames')
 
 interface GalleryFilterProps {
-  render: (imgs: any[]) => JSX.Element
+  // render: (imgs: any[]) => JSX.Element
+  handleChange: (e: any) => void
+  json: any
+  checkbox: any
 }
 
-const GalleryFilter = ({ render }: GalleryFilterProps) => {
+const GalleryFilter = ({ checkbox, handleChange, json }: GalleryFilterProps) => {
   const bp = useMedia(['(min-width: 750px)'], [true], false)
-  const [json, setjson] = useState()
-  const isMounted = useRef(false)
-  const initJson = useRef<any[]>([])
-  const [imageFilter, setImageFilter] = useState<any[]>([])
-  const [checkbox] = useState(new Map())
 
-  useEffect(() => {
-    isMounted.current = true
-
-    fetch(`${process.env.PUBLIC_URL}/img.json`)
-      .then(async res => {
-        const r = await res.json()
-        const { gallery } = r
-        isMounted.current && setjson(gallery)
-
-        let imgs: any[] = []
-        isMounted.current &&
-          Object.keys(gallery).forEach(key => {
-            const category = gallery[key]
-            imgs = imgs.concat(category)
-          })
-        initJson.current = imgs
-        setImageFilter(imgs)
-      })
-      .catch()
-
-    return () => {
-      isMounted.current = false
-    }
-  }, [])
-
-  const merge = (a: any[], b: any[]) => a.filter(aa => !b.find(bb => aa.src === bb.src)).concat(b)
-
-  const handleChange = (e: any) => {
-    const label = e.target.value
-    const check = checkbox
-    if (check.has(label)) {
-      check.delete(label)
-    } else {
-      check.set(label, json[label])
-    }
-    arrangeFilter()
-  }
-
-  const arrangeFilter = () => {
-    let filter: any[] = []
-    const check = checkbox
-    check.forEach(ar => {
-      filter = merge(filter, ar)
-    })
-    filter.length > 0 ? setImageFilter(filter) : setImageFilter(initJson.current)
-  }
 
   return (
     <div className="galleryWrapper">
@@ -99,7 +51,7 @@ const GalleryFilter = ({ render }: GalleryFilterProps) => {
           </form>
         )}
       </div>
-      {render(imageFilter)}
+      {/* {render(imageFilter)} */}
     </div>
   )
 }
